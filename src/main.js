c5d5c11a1d27e9e9bea00e26cfeec470869aac41
LAPTOP-UOABPOLV\ASUS <1060696988@qq.com>
2022-12-13 23:24:52 +0800

    整年图表和整月图表

--- src/main.js
+++ src/main.js
@@ -2,8 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import dataV from '@jiaminghi/data-view'
+import * as echarts from 'echarts'
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios'
+import 'echarts-liquidfill'
+import VueRouter from 'vue-router'
+import ElementUi from 'element-ui';
 
+Vue.prototype.$echarts = echarts
 Vue.config.productionTip = false
+
+Vue.prototype.axios = axios
+
+Vue.use(dataV)
+
+Vue.use(VueRouter)
+
+Vue.use(ElementUi)
 
 new Vue({
   router,
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,64 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="demo">
+    <InfectionYearStateChart
+      ref="StateChart1"
+      :data="dataList1"
+      style="width: 100%; height: 380px"
+    />
+    </div>
+    <div class="demo">
+    <InfectionMonthStateChart
+      ref="StateChart2"
+      :data="dataList2"
+      style="width: 100%; height: 380px"
+    />
+    </div>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+// 引入组件
+import InfectionYearStateChart from '../components/Histogram/InfectionYearStateChart.vue'
+import InfectionMonthStateChart from '../components/Histogram/InfectionMonthStateChart.vue'
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+data(){
+        return{
+          dataList1: {
+            choice:1,//选择
+            place:'中国',//地区
+            year:2020,
+            valueList: //二维数组，0为总人数，1为治愈人数，2为死亡人数
+            [[68188, 35379,20100,23452,54312,14563,85923,54367,34567,12345,12345,56745],
+            [88188, 35379,20100,23452,54312,14563,85923,54367,34567,12345,12345,56745],
+            [48188, 35379,20100,23452,54312,14563,85923,54367,34567,12345,12345,56745]],
+            forecastList:[]
+          },
+          dataList2:{
+            choice:0,
+            place:'中国',
+            year:2020,
+            month:4,
+            valueList:[[68188, 35379,20100,23452,54312,14563,85923,54367,34567,12345,12345,56745,
+            88188, 35379,20100,23452,54312,14563,85923,54367,34567,12345,12345,56745,
+            88188, 35379,20100,23452,54312,14563],
+            [88188, 35379,20100,23452,54312,14563,85923,54367,34567,12345,12345,56745],
+            [48188, 35379,20100,23452,54312,14563,85923,54367,34567,12345,12345,56745]],
+          }
+        }
+    },
+    mounted () {
+       this.$refs.StateChart1.initChart();
+       this.$refs.StateChart2.initChart();
+    },
+    components: {
+       InfectionYearStateChart,
+       InfectionMonthStateChart
+    }
 }
 </script>
+<style>
+.demo {
+  width: 500px;
+  height: 600px;
+}
+</style>
